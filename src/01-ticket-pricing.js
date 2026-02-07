494e39b6358fb262da9ba8e77e2ef366c1b14759
Ashutosh Singh <singhashutosh4123@gmail.com>
2026-02-08 00:32:13 +0530

    comp: covered all cases

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -22,6 +22,29 @@
  * @param {boolean} isWeekend - Whether it's a weekend
  * @returns {number} The ticket price, or -1 for invalid input
  */
+
+// code below
+
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+  let ticketPrice = 0;
+  if(age < 0){
+    ticketPrice = -1;
+  } else if(age <= 12 && age >= 0){
+    ticketPrice += 8;
+  } else if(age <= 17 && age >= 13){
+    ticketPrice += 12;
+  } else if(age <= 59 && age >= 18){
+    ticketPrice += 15;
+  } else if(age >= 60){
+    ticketPrice += 10;
+  } else{
+    ticketPrice = -1;
+  }
+
+  if(ticketPrice != -1){
+    if(isWeekend){
+      ticketPrice += 3;
+    } 
+  } 
+    return ticketPrice;
 }
